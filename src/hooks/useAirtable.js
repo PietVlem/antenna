a823010971c7b1330f77c1fb424d2b506858d475
Pieter <pieter.vlem@gmail.com>
2022-06-15 02:10:25 +0200

    [Task] code refactoring

--- src/hooks/useAirtable.js
+++ src/hooks/useAirtable.js
@@ -2,24 +2,21 @@ import Airtable from 'airtable'
 
 export default function useAirtable() {
     function createAirtableRecord(data) {
-        console.log(data)
         const base = new Airtable({ apiKey: import.meta.env.VITE_AIRTABLE_API_KEY }).base('apprAc6HQk9NOfd3v');
 
         const payload = {
             "Full Name": data['contact-first-name'] + ' ' + data['contact-name'],
-            "Email": data['contact-email'] ? data['contact-email'] : '',
-            "Phone": data['contact-phone'] ? data['contact-phone'] : '',
+            "Email": data['contact-email'] || '',
+            "Phone": data['contact-phone'] || '',
             "newsletter": data['contact-newsletter'] ? true : false,
-            "Question or remark": data['contact-question'] ? data['contact-question'] : '',
+            "Question or remark": data['contact-question'] || '',
         }
-
-        console.log(payload)
 
         base('Teams').create([
             {
                 "fields": payload
             }
-        ], function (err, records) {
+        ], (err, records) => {
             if (err) {
                 console.error(err);
                 return;
